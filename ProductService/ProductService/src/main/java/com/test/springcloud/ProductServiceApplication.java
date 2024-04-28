2d2f1ad6e166f23088e43394eb6a9fc5ec572bbe
pradeep <k.pradeep0491@gmail.com>
2024-04-28 12:32:02 +0530

    changes

--- ProductService/ProductService/src/main/java/com/test/springcloud/ProductServiceApplication.java
+++ ProductService/ProductService/src/main/java/com/test/springcloud/ProductServiceApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ProductServiceApplication {
@@ -11,6 +12,11 @@ public class ProductServiceApplication {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    WebClient webClient(){
+        return WebClient.builder().build();
     }
 
     public static void main(String[] args) {
